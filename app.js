adb3a71bb2ba1d9c5c8c90e066bbff2fc4e3732c
claytonhenrylewis <claytonhenrylewis@gmail.com>
2017-11-29 18:37:13 -0500

    Start work on populating playlists

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.get('/callback', function(req, res) {
             refresh_token = body.refresh_token;
 
         var options = {
-          url: 'https://api.spotify.com/v1/me',
+          url: 'https://api.spotify.com/v1/me/playlists',
           headers: { 'Authorization': 'Bearer ' + access_token },
           json: true
         };
